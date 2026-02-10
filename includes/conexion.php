af168527b7c2e5c4f54f1bba54f4433b22646b37
AlbertoGonzález <albertogonzlezcorp@gmail.com>
2026-02-10 12:20:04 +0100

    Conexión base de datos definitiva

--- includes/conexion.php
+++ includes/conexion.php
@@ -7,11 +7,15 @@ $base_datos = "fitness_db";
 
 //Funcionalidades de PDO
 $db_options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_EMULATE_PREPARES   => false,
 );
 
-
-
+try {
+    $pdo = new PDO("mysql:host=$servidor;dbname=$base_datos;charset=utf8", $usuario, $password,$db_options);
+} catch (PDOException $e) {
+    echo "Error de conexión a la base de datos: ", $e->getMessage();
+    exit;
+}
 ?>
