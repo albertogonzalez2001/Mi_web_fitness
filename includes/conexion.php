53c6cbde2c6cd985f27eb51fa6e1554dc6a9d2d4
AlbertoGonzález <albertogonzlezcorp@gmail.com>
2026-02-10 12:17:54 +0100

    conexión a la base de datos establecida

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,10 +1,16 @@
 <?php
+//Variables de la base de datos
 $servidor = "localhost";
 $usuario = "root";
 $password = "";
 $base_datos = "fitness_db";
 
-
+//Funcionalidades de PDO
+$db_options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+);
 
 
 
